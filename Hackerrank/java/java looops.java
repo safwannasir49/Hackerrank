689e2deff12867587236180ae11c66f03e7eece8
safwannasir49 <safwannasir49@gmail.com>
2024-05-11 20:05:14 +0530

    first commit

---
+++ Hackerrank/java/java looops.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int  t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j=0;j<n;j++){
+            a = (int) (a + Math.pow(2,j)*b);
+            System.out.print(a +" "); 
+        }
+        System.out.print("\n");
+        }
+        in.close();
+    }
+}
